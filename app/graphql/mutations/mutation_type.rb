c9ba214b67c65dde9891d07d8ea96841a9819dc3
DANIEL VISCA <danielvisca96@gmail.com>
2019-02-06 01:08:23 -0500

    Purchasing carts given an id is not implemented

--- app/graphql/mutations/mutation_type.rb
+++ app/graphql/mutations/mutation_type.rb
@@ -6,8 +6,31 @@ Mutations::MutationType = GraphQL::ObjectType.define do
     argument :id, !types.ID
     resolve ->(obj, args, ctx){
       product = Product.find_by_id(args[:id])
-      product.update(inventory_count: 0)
-      product
+      inventory_count = product.inventory_count
+      if inventory_count == 0
+        product
+      else
+        product.update(inventory_count: inventory_count-1)
+        product
+      end
+    }
+  end
+
+  field :purchaseCart, !types[Types::CartType] do
+    description 'Purchase every product in a cart given the cart ID'
+    argument :id, !types.ID
+    resolve ->(obj,args, ctx) {
+      cart = CartItem.where(cart_id: Cart.find_by_id(args[:id]))
+      cart.each do |item|
+        product = Product.find_by_id(item.product_id)
+        inventory_count = product.inventory_count
+        if inventory_count == 0
+          next
+        else
+          product.update(inventory_count: inventory_count -1)
+        end
+      end
+    cart
     }
   end
 end
